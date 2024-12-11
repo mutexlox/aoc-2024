493d685223ed735bc4e11fb958bd468134bcb424
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2024-12-10 19:59:46 -0500

    Do day03

---
+++ src/day03.rs
@@ -0,0 +1,78 @@
+use anyhow::Result;
+use aoc_2024::util;
+use once_cell::sync::Lazy;
+use regex::Regex;
+use std::str::FromStr;
+use std::vec::Vec;
+
+#[derive(Debug)]
+enum Ops {
+    Mul(u64, u64),
+    Disable,
+    Enable,
+}
+
+#[derive(Debug)]
+struct Computer {
+    ops: Vec<Ops>,
+    enabled: bool,
+    pub ignore_disable: bool,
+}
+
+impl Computer {
+    fn new(ops: Vec<Ops>) -> Self {
+        Self {
+            ops,
+            enabled: true,
+            ignore_disable: false,
+        }
+    }
+
+    fn evaluate(&mut self) -> u64 {
+        let mut out = 0;
+        for op in &self.ops {
+            match op {
+                Ops::Mul(lhs, rhs) => {
+                    if self.enabled || self.ignore_disable {
+                        out += lhs * rhs
+                    }
+                }
+                Ops::Disable => self.enabled = false,
+                Ops::Enable => self.enabled = true,
+            }
+        }
+        out
+    }
+}
+
+impl FromStr for Computer {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self> {
+        static RE: Lazy<Regex> =
+            Lazy::new(|| Regex::new(r"mul\((?<lhs>\d+),(?<rhs>\d+)\)|do\(\)|don't\(\)").unwrap());
+        let mut out = Vec::new();
+        for captures in RE.captures_iter(s) {
+            let full = captures.get(0).unwrap().as_str();
+            if full == "don't()" {
+                out.push(Ops::Disable);
+            } else if full == "do()" {
+                out.push(Ops::Enable);
+            } else {
+                out.push(Ops::Mul(
+                    captures["lhs"].parse::<u64>()?,
+                    captures["rhs"].parse::<u64>()?,
+                ));
+            }
+        }
+        Ok(Computer::new(out))
+    }
+}
+
+fn main() {
+    let contents = util::get_all_input();
+    let mut parsed = contents.parse::<Computer>().unwrap();
+    parsed.ignore_disable = true;
+    println!("evaluated pt 1: {}", parsed.evaluate());
+    parsed.ignore_disable = false;
+    println!("evaluated pt 2: {}", parsed.evaluate());
+}
--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::fs::File;
+use std::fs::{read_to_string, File};
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
@@ -8,4 +8,9 @@ pub fn get_lines() -> std::io::Lines<BufReader<File>> {
     let path = Path::new(&arg);
     let file = File::open(path).unwrap_or_else(|_| panic!("could not open {}", path.display()));
     BufReader::new(file).lines()
+}
+
+pub fn get_all_input() -> String {
+    let arg = env::args().nth(1).expect("Need file to read");
+    read_to_string(&arg).unwrap_or_else(|_| panic!("Could not read {}", arg))
 }
