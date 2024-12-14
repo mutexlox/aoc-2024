f5a22d3d14563b4cdb0d586977b94baaa5d8726f
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2024-12-13 21:11:44 -0500

    Fix day 12

--- src/day12.rs
+++ src/day12.rs
@@ -1,5 +1,5 @@
 use aoc_2024::util;
-use std::collections::VecDeque;
+use std::collections::{HashMap, HashSet, VecDeque};
 
 fn sum_costs(garden: &[Vec<char>]) -> u64 {
     let mut visited = vec![vec![false; garden[0].len()]; garden.len()];
@@ -50,6 +50,48 @@ fn sum_costs(garden: &[Vec<char>]) -> u64 {
     cost
 }
 
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+type HashKey = (usize, usize, Direction);
+
+fn find(union_find: &mut HashMap<HashKey, (HashKey, u64)>, mut a: HashKey) -> HashKey {
+    let mut root = a;
+    while union_find[&root].0 != root {
+        root = union_find[&root].0;
+    }
+
+    while union_find[&a].0 != root {
+        let parent = union_find[&a].0;
+        union_find.insert(a, (root, union_find[&a].1));
+        a = parent;
+    }
+    root
+}
+
+fn union(union_find: &mut HashMap<HashKey, (HashKey, u64)>, a: HashKey, b: HashKey) {
+    let a = find(union_find, a);
+    let b = find(union_find, b);
+    if a == b {
+        return;
+    }
+
+    let a_size = union_find[&a].1;
+    let b_size = union_find[&b].1;
+    if a_size < b_size {
+        union_find.insert(b, (a, b_size));
+        union_find.insert(a, (union_find[&a].0, a_size + b_size));
+    } else {
+        union_find.insert(a, (b, a_size));
+        union_find.insert(b, (union_find[&b].0, a_size + b_size));
+    }
+}
+
 fn sum_bulk_costs(garden: &[Vec<char>]) -> u64 {
     let mut visited = vec![vec![false; garden[0].len()]; garden.len()];
     let mut cost = 0;
@@ -61,8 +103,9 @@ fn sum_bulk_costs(garden: &[Vec<char>]) -> u64 {
             // bfs from here for extent
             let mut queue = VecDeque::new();
             queue.push_back((i, j));
-            let mut sides = 0;
             let mut area = 0;
+
+            let mut union_find = HashMap::new();
 
             while let Some((next_i, next_j)) = queue.pop_front() {
                 if visited[next_i][next_j] {
@@ -71,160 +114,87 @@ fn sum_bulk_costs(garden: &[Vec<char>]) -> u64 {
                 visited[next_i][next_j] = true;
 
                 area += 1;
+
                 if next_i > 0 && garden[next_i - 1][next_j] == start {
                     queue.push_back((next_i - 1, next_j));
                 } else {
-                    // check if left or right neighbor are visited && have this neighbor above; if not add 1 to sides
-                    let mut continue_side = false;
-                    if (next_j > 0
-                        && visited[next_i][next_j - 1]
-                        && garden[next_i][next_j - 1] == start)
-                        && (next_i == 0 || garden[next_i - 1][next_j - 1] != start)
-                    {
-                        println!("line 80");
-                        continue_side = true;
+                    let key = (next_i, next_j, Direction::Up);
+                    union_find.insert(key, (key, 1));
+
+                    if next_j > 0 && union_find.contains_key(&(next_i, next_j - 1, Direction::Up)) {
+                        union(&mut union_find, key, (next_i, next_j - 1, Direction::Up));
                     }
-                    if (next_j < garden[0].len() - 1
-                        && visited[next_i][next_j + 1]
-                        && garden[next_i][next_j + 1] == start)
-                        && (next_i == 0 || garden[next_i - 1][next_j + 1] != start)
-                    {
-                        println!("line 83");
-                        continue_side = true;
-                    }
-                    println!(
-                        "... for {} at {},{} up border continues a side? {}",
-                        start, next_i, next_j, continue_side
-                    );
-                    if !continue_side {
-                        sides += 1;
+
+                    if union_find.contains_key(&(next_i, next_j + 1, Direction::Up)) {
+                        union(&mut union_find, key, (next_i, next_j + 1, Direction::Up));
                     }
                 }
 
                 if next_i < garden.len() - 1 && garden[next_i + 1][next_j] == start {
                     queue.push_back((next_i + 1, next_j));
                 } else {
-                    // check if left or right neighbor are visited && have this neighbor below; if not add 1 to sides
-                    let mut continue_side = false;
-                    if (next_j > 0
-                        && visited[next_i][next_j - 1]
-                        && garden[next_i][next_j - 1] == start)
-                        && (next_i == garden.len() - 1 || garden[next_i + 1][next_j - 1] != start)
+                    let key = (next_i, next_j, Direction::Down);
+
+                    union_find.insert(key, (key, 1));
+
+                    if next_j > 0 && union_find.contains_key(&(next_i, next_j - 1, Direction::Down))
                     {
-                        println!("line 97");
-                        continue_side = true;
+                        union(&mut union_find, key, (next_i, next_j - 1, Direction::Down));
                     }
-                    if (next_j < garden[0].len() - 1
-                        && visited[next_i][next_j + 1]
-                        && garden[next_i][next_j + 1] == start)
-                        && (next_i == garden.len() - 1 || garden[next_i + 1][next_j + 1] != start)
-                    {
-                        println!("line 100");
-                        continue_side = true;
-                    }
-                    println!(
-                        "... for {} at {},{} down border continues a side? {}",
-                        start, next_i, next_j, continue_side
-                    );
-                    if !continue_side {
-                        sides += 1;
+
+                    if union_find.contains_key(&(next_i, next_j + 1, Direction::Down)) {
+                        union(&mut union_find, key, (next_i, next_j + 1, Direction::Down));
                     }
                 }
 
                 if next_j > 0 && garden[next_i][next_j - 1] == start {
                     queue.push_back((next_i, next_j - 1));
                 } else {
-                    // check if up or down neighbor are visited && have this neighbor left; if not add 1 to sides
-                    let mut continue_side = false;
-                    if (next_i > 0
-                        && visited[next_i - 1][next_j]
-                        && garden[next_i - 1][next_j] == start)
-                        && (next_j == 0 || garden[next_i - 1][next_j - 1] != start)
+                    let key = (next_i, next_j, Direction::Left);
+
+                    union_find.insert(key, (key, 1));
+
+                    if next_i > 0 && union_find.contains_key(&(next_i - 1, next_j, Direction::Left))
                     {
-                        println!("line 114");
-                        continue_side = true;
+                        union(&mut union_find, key, (next_i - 1, next_j, Direction::Left));
                     }
-                    if (next_i < garden.len() - 1
-                        && visited[next_i + 1][next_j]
-                        && garden[next_i + 1][next_j] == start)
-                        && (next_j == 0 || garden[next_i + 1][next_j - 1] != start)
-                    {
-                        println!("line 117XXX");
-                        continue_side = true;
-                    }
-                    println!(
-                        "... for {} at {},{} left border continues a side? {}",
-                        start, next_i, next_j, continue_side
-                    );
-                    if !continue_side {
-                        sides += 1;
+
+                    if union_find.contains_key(&(next_i + 1, next_j, Direction::Left)) {
+                        union(&mut union_find, key, (next_i + 1, next_j, Direction::Left));
                     }
                 }
 
                 if next_j < garden[0].len() - 1 && garden[next_i][next_j + 1] == start {
                     queue.push_back((next_i, next_j + 1));
                 } else {
-                    // check if up or down neighbor are visited && have this neighbor right; if not add 1 to sides
-                    let mut continue_side = false;
-                    if (next_i > 0
-                        && visited[next_i - 1][next_j]
-                        && garden[next_i - 1][next_j] == start)
-                        && (next_j == garden[0].len() - 1
-                            || garden[next_i - 1][next_j + 1] != start)
+                    let key = (next_i, next_j, Direction::Right);
+
+                    union_find.insert(key, (key, 1));
+
+                    if next_i > 0
+                        && union_find.contains_key(&(next_i - 1, next_j, Direction::Right))
                     {
-                        println!("line 131");
-                        continue_side = true;
+                        union(&mut union_find, key, (next_i - 1, next_j, Direction::Right));
                     }
-                    if (next_i < garden.len() - 1
-                        && visited[next_i + 1][next_j]
-                        && garden[next_i + 1][next_j] == start)
-                        && (next_j == garden[0].len() - 1
-                            || garden[next_i + 1][next_j + 1] != start)
-                    {
-                        println!("line 134XXX");
-                        continue_side = true;
-                    }
-                    println!(
-                        "... for {} at {},{} right border continues a side? {}",
-                        start, next_i, next_j, continue_side
-                    );
-                    if !continue_side {
-                        sides += 1;
+
+                    if union_find.contains_key(&(next_i + 1, next_j, Direction::Right)) {
+                        union(&mut union_find, key, (next_i + 1, next_j, Direction::Right));
                     }
                 }
             }
 
-            println!(
-                "for {}: cost {}, area {}, sides {}",
-                start,
-                area * sides,
-                area,
-                sides
-            );
+            let mut roots = HashSet::new();
+            let keys = union_find.keys().cloned().collect::<Vec<_>>();
+            for &key in keys.iter() {
+                roots.insert(find(&mut union_find, key));
+            }
+
+            let sides = roots.len() as u64;
+
             cost += area * sides;
         }
     }
     cost
-}
-
-fn rotate_90(garden: &[Vec<char>]) -> Vec<Vec<char>> {
-    let mut out = vec![vec![' '; garden.len()]; garden[0].len()];
-    for (i, row) in garden.iter().enumerate() {
-        for (j, &item) in row.iter().enumerate() {
-            out[j][garden.len() - 1 - i] = item;
-        }
-    }
-    out
-}
-
-fn mirror(garden: &[Vec<char>]) -> Vec<Vec<char>> {
-    let mut out = vec![vec![' '; garden[0].len()]; garden.len()];
-    for (i, row) in garden.iter().enumerate() {
-        for (j, &item) in row.iter().enumerate() {
-            out[garden.len() - 1 - i][j] = item;
-        }
-    }
-    out
 }
 
 fn main() {
@@ -233,33 +203,5 @@ fn main() {
         garden.push(line.chars().collect());
     }
     println!("{}", sum_costs(&garden));
-    let bulk = sum_bulk_costs(&garden);
-    println!("{}", bulk);
-
-    let rotated_90 = rotate_90(&garden);
-    //assert_eq!(bulk, sum_bulk_costs(&rotated_90));
-    let rotated_180 = rotate_90(&rotated_90);
-    //assert_eq!(bulk, sum_bulk_costs(&rotated_180));
-    let rotated_270 = rotate_90(&rotated_180);
-    //assert_eq!(bulk, sum_bulk_costs(&rotated_270));
-    let mirror = mirror(&garden);
-    //assert_eq!(bulk, sum_bulk_costs(&mirror));
-    println!(
-        "\n\n =============================== rotated 90 =============================== \n\n"
-    );
-    let costs_90 = sum_bulk_costs(&rotated_90);
-    println!(
-        "\n\n =============================== rotated 180 =============================== \n\n"
-    );
-    let costs_180 = sum_bulk_costs(&rotated_180);
-    println!(
-        "\n\n =============================== rotated 270 =============================== \n\n"
-    );
-    let costs_270 = sum_bulk_costs(&rotated_270);
-    println!("\n\n =============================== mirrored =============================== \n\n");
-    let costs_mirror = sum_bulk_costs(&mirror);
-    println!(
-        "normal: {}  90: {} 180: {} 270: {} mirror {}",
-        bulk, costs_90, costs_180, costs_270, costs_mirror
-    );
+    println!("{}", sum_bulk_costs(&garden));
 }
