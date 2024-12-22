6d80120049304190ff5dfa5ee812b792dfca4ba9
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2024-12-22 12:41:26 -0500

    Do day 22

---
+++ src/day22.rs
@@ -0,0 +1,46 @@
+use aoc_2024::util;
+use std::collections::{HashMap, VecDeque};
+
+const MODULO: i64 = 16_777_216;
+
+fn prng_step(x: i64) -> i64 {
+    let x = ((x * 64) ^ x) & (MODULO - 1);
+    let x = ((x / 32) ^ x) & (MODULO - 1);
+    ((x * 2048) ^ x) & (MODULO - 1)
+}
+
+// map is a map of last 4 changes -> sum of what you can get
+fn run_steps(map: &mut HashMap<[i64; 4], i64>, mut x: i64, steps: usize) -> i64 {
+    let mut last_n = VecDeque::new();
+    let mut map_local = HashMap::new();
+    for _ in 0..steps {
+        let next = prng_step(x);
+        if last_n.len() == 4 {
+            last_n.pop_front();
+        }
+        last_n.push_back(next % 10 - x % 10);
+        x = next;
+        if last_n.len() == 4 {
+            let to_add = [last_n[0], last_n[1], last_n[2], last_n[3]];
+            map_local.entry(to_add).or_insert(next % 10);
+        }
+    }
+    for (&k, &v) in map_local.iter() {
+        *map.entry(k).or_default() += v;
+    }
+    x
+}
+
+fn run_steps_and_sum(seeds: &[i64], steps: usize) -> (i64, i64) {
+    let mut map = HashMap::new();
+    let sum = seeds.iter().map(|&x| run_steps(&mut map, x, steps)).sum();
+    (sum, *map.values().max().unwrap())
+}
+
+fn main() {
+    let seeds = util::get_lines()
+        .map_while(Result::ok)
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+    println!("{:?}", run_steps_and_sum(&seeds, 2000));
+}
