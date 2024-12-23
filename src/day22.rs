022a21c26e2a136d7b725f1931bda1e6b403600e
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2024-12-22 21:42:55 -0500

    22: Slightly simplify RNG code and hash key.

--- src/day22.rs
+++ src/day22.rs
@@ -4,13 +4,19 @@ use std::collections::{HashMap, VecDeque};
 const MODULO: i64 = 16_777_216;
 
 fn prng_step(x: i64) -> i64 {
-    let x = ((x * 64) ^ x) & (MODULO - 1);
-    let x = ((x / 32) ^ x) & (MODULO - 1);
-    ((x * 2048) ^ x) & (MODULO - 1)
+    let x = ((x << 6) ^ x) & (MODULO - 1);
+    let x = ((x >> 5) ^ x) & (MODULO - 1);
+    ((x << 11) ^ x) & (MODULO - 1)
+}
+
+// Assumes each element x is -9 <= x <= 9
+fn hash_key(arr: [i64; 4]) -> i64 {
+    // range 0 - 18; 5 bits
+    (arr[0] + 9) | (arr[1] + 9) << 5 | (arr[2] + 9) << 10 | (arr[3] + 9) << 15
 }
 
 // map is a map of last 4 changes -> sum of what you can get
-fn run_steps(map: &mut HashMap<[i64; 4], i64>, mut x: i64, steps: usize) -> i64 {
+fn run_steps(map: &mut HashMap<i64, i64>, mut x: i64, steps: usize) -> i64 {
     let mut last_n = VecDeque::new();
     let mut map_local = HashMap::new();
     for _ in 0..steps {
@@ -22,7 +28,7 @@ fn run_steps(map: &mut HashMap<[i64; 4], i64>, mut x: i64, steps: usize) -> i64 
         x = next;
         if last_n.len() == 4 {
             let to_add = [last_n[0], last_n[1], last_n[2], last_n[3]];
-            map_local.entry(to_add).or_insert(next % 10);
+            map_local.entry(hash_key(to_add)).or_insert(next % 10);
         }
     }
     for (&k, &v) in map_local.iter() {
