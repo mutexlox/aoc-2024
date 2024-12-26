4c5136d9c868edd74e7681230379bc366950cfbb
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2024-12-25 21:06:23 -0500

    Day 17: minor speed optimizations

--- src/day07.rs
+++ src/day07.rs
@@ -23,21 +23,29 @@ fn is_satisfiable_add_mul(equation: &[i64]) -> bool {
 
 fn is_satisfiable_add_mul_concat(equation: &[i64]) -> bool {
     let (target, rest) = (equation[0], &equation[1..]);
+    // Precompute logs and powers
+    let multipliers = rest
+        .iter()
+        .skip(1)
+        .map(|v| 10_i64.pow(v.ilog10() + 1))
+        .collect::<Vec<_>>();
     for combination in 0..3_usize.pow((rest.len() - 1) as u32) {
         let mut tmp = rest[0];
-        for (i, val) in rest.iter().skip(1).enumerate() {
+        let mut combination = combination;
+        for (val, mul) in rest.iter().skip(1).zip(multipliers.iter()) {
             if tmp > target {
                 break;
             }
-            match (combination / 3_usize.pow(i as u32)) % 3 {
+            match combination % 3 {
                 0 => tmp *= val,
                 1 => tmp += val,
                 2 => {
-                    tmp *= 10_i64.pow(val.ilog10() + 1);
+                    tmp *= mul;
                     tmp += val;
                 }
                 _ => panic!("uh, math doesn't work"),
             }
+            combination /= 3;
         }
         if tmp == target {
             return true;
